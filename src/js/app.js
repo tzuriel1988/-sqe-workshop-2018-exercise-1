a597af94312c150faefdadd1bb761040940fbcce
tzuriel1988 <tzuri1988@gmail.com>
2019-01-01 21:48:12 +0200

    merge

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
-import {parserNewJson,symbolicSubstition,assignmentArguments,getArrayColors} from './Parser';
+import {parserNewJson} from './Parser';
 
 var ansTable = document.getElementById('Table');
 var correntRow;
-let dic={};
 var tableRows = ansTable.getElementsByTagName('tr');
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
@@ -18,22 +17,14 @@ $(document).ready(function () {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
-        let arrayTable=parserNewJson(parsedCode,dic,0);
-        createTable(arrayTable);
-        assignmentArguments(document.getElementById('args').value);
-        var symbolicFun=symbolicSubstition(codeToParse,dic);
-        document.getElementById('symbolicFunction').innerHTML = '';
-        createSymbolicFun(symbolicFun);
+        let arrayTable=parserNewJson(parsedCode);
+        var i=0;
+        for (i=0;i<arrayTable.length;i=i+5){
+            addRowToTable(arrayTable[i], arrayTable[i+1], arrayTable[i+2], arrayTable[i+3], arrayTable[i+4]);
+            correntRow=ansTable.insertRow(ansTable.rows.length);
+        }
     });
 });
-
-
-function createTable(arrayTable){
-    for (var i=0;i<arrayTable.length;i=i+5){
-        addRowToTable(arrayTable[i], arrayTable[i+1], arrayTable[i+2], arrayTable[i+3], arrayTable[i+4]);
-        correntRow=ansTable.insertRow(ansTable.rows.length);
-    }
-}
 
 
 function addRowToTable(line,type,name,condition,value){
@@ -51,48 +42,4 @@ function addColToTable(col,value) {
     var correntCol  = correntRow.insertCell(col);
     var ValueOfCol  = document.createTextNode(value);
     correntCol.appendChild(ValueOfCol);
-}
-
-function createSymbolicFun(fun) {
-    var arrayOfColors=getArrayColors();
-    var lineOfIf=0;
-    var color;
-    for (let i=0;i<fun.length;i++) {
-        var linesOfFun=fun[i];
-        if(linesOfFun.includes('if')){
-            var isColor=arrayOfColors[lineOfIf];
-            lineOfIf++;
-            if(isColor)
-                color= 'lawngreen';
-            else
-                color= 'darkred';
-        }
-        else
-            color='white';
-        drow(linesOfFun,color);
-    }
-}
-
-function drow(linesOfFun,color){
-    if(linesOfFun.includes('<')){
-        linesOfFun=createSpeace(linesOfFun);
-    }
-    $('#symbolicFunction').append(
-        $('<div>' + linesOfFun + '</div>').addClass(color)
-    );
-}
-
-function createSpeace(linesOfFun){
-    var ans='';
-    var lineSplited=linesOfFun.split('<');
-    for (let i=0;i<lineSplited.length;i++) {
-        ans=ans+lineSplited[i]+' < ';
-    }
-    ans=ans.substring(0,ans.length-3);
-
-    return ans;
-
-
-
-
 }
